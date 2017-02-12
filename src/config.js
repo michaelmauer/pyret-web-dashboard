10135e59520c92698feab9a91e6cbaa40aa2026c
kevinschaich <schaich.kevin@gmail.com>
2017-02-12 13:25:16 -0500

    Updated scope for file access

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@
 export const APP_NAME = 'Pyret';
 
 // Client ID and API key from the Developer Console
+export const API_KEY = 'AIzaSyD0f_a15htB0Ano6RDwcUUwInf5ZsiSMiM';
 export const CLIENT_ID = '616482134011-3hnr36kvc6jeh1dfojv3gblj8bu7i6b7.apps.googleusercontent.com';
 
 // Array of API discovery doc URLs for APIs used by the quickstart
@@ -9,7 +10,7 @@ export const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/dri
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
-export const SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+export const SCOPES = 'https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/drive.appfolder https://www.googleapis.com/auth/drive.file';
 
 // File extension of code files for this app.
 export const FILE_EXT = 'arr';
